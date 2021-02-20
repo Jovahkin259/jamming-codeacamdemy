f552292ed30ea839ad98e6e3c77d3f02b5a4be3d
Joshua Astle <jovahkin259@gmail.com>
2021-02-20 16:42:09 +0000

    Lint files & fixed Track + - bug

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -4,18 +4,20 @@ import './Track.css'
 
 class Track extends React.Component {
   renderAction () {
-    return this.props.isRemoval ? <button className="Track-action">-</button> : <button className="Track-action">-</button>
+    return this.props.isRemoval
+      ? <button className="Track-action">-</button>
+      : <button className="Track-action">+</button>
   }
 
   render () {
     return (
             <div className="Track">
-  <div className="Track-information">
-    {/* <h3><!-- track name will go here --></h3> */}
-    {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
-  </div>
-  {/* <button class="Track-action"><!-- + or - will go here --></button> */}
-</div>
+              <div className="Track-information">
+                <h3> track name </h3>
+                <p>track artist | track album </p>
+              </div>
+                {this.renderAction}
+            </div>
     )
   }
 }
